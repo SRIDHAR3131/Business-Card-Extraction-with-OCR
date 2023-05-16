6ed8c6326314e7434bb25c61e302b98e2e7effa2
SRIDHAR R <68391060+SRIDHAR3131@users.noreply.github.com>
2023-05-16 22:40:46 +0530

    main

--- bizcard.py
+++ bizcard.py
@@ -77,7 +77,7 @@ with text_process:
 
             with left:
                 st.markdown("### Welcome to the Business Card Application!")
-                st.markdown('###### Bizcard is a Python application designed to extract information from business cards. It utilizes various technologies such as :blue[Streamlit, Python, EasyOCR , RegEx function, OpenCV, and MySQL] database to achieve this functionality.')
+                st.markdown('###### Bizcard is a Python application designed to extract information from business cards. It utilizes various technologies such as :blue[Streamlit, Streamlit_lottie, Python, EasyOCR , RegEx function, OpenCV, and MySQL] database to achieve this functionality.')
                 st.write('The main purpose of Bizcard is to automate the process of extracting key details from business card images, such as the name, designation, company, contact information, and other relevant data. By leveraging the power of OCR (Optical Character Recognition) provided by EasyOCR, Bizcard is able to extract text from the images.')
                 st.write("Click on the ****:red[Image to text]**** option to start exploring the Bizcard extraction.")
 
